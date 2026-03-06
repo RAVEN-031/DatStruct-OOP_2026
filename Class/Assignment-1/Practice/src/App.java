e129c997785e027631f6087ae4ae58e44cbe3d83
RAVEN-031 <izzatwahyudi07@gmail.com>
2026-03-06 09:05:57 +0700

    Base for assignment 1

--- Class/Assignment-1/Practice/src/App.java
+++ Class/Assignment-1/Practice/src/App.java
@@ -3,3 +3,51 @@ public class App {
         System.out.println("Jawa!");
     }
 }
+
+abstract class Firearm {
+
+    //Attributes
+    protected String origins;
+    protected int capacity;
+    protected String calibre;
+    protected int firerate;
+
+    //Constructor
+    public Firearm(String origins, int capacity, String calibre, int firerate){
+        this.origins = origins;
+        this.capacity = capacity;
+        this.calibre = calibre;
+        this.firerate = firerate;
+    }
+
+    //Setter
+    public void setOrigins(String country){
+        this.origins = country;
+    }
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
+    public void setCalibre(String calibre){
+        this.calibre = calibre;
+    }
+    public void setFirerate(int firerate){
+        this.firerate = firerate;
+    }
+    
+    //getter
+    public String getOrigins(){
+        return this.origins;
+    }
+    public int getCapacity(){
+        return this.capacity;
+    }
+    public String getCalibre(){
+        return this.calibre;
+    }
+    public int getFirerate(){
+        return this.firerate;
+    }
+
+    //Methods
+    
+}
